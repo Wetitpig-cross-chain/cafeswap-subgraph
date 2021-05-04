8dfa18862eba290491a8fbdaa3057a0fcdde899f
Wetitpig <winsto003@hotmail.com>
2021-05-04 22:43:12 +0800

    Add mdex bsc version

--- src/mappings/helpers.ts
+++ src/mappings/helpers.ts
@@ -6,7 +6,7 @@ import { ERC20NameBytes } from '../types/Factory/ERC20NameBytes'
 import { Factory as FactoryContract } from '../types/templates/Pair/Factory'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-export const FACTORY_ADDRESS = '0x3e708FdbE3ADA63fc94F8F61811196f1302137AD'
+export const FACTORY_ADDRESS = '0x3CD1C46068dAEa5Ebb0d3f55F6915B10648062B8'
 
 export let ZERO_BI = BigInt.fromI32(0)
 export let ONE_BI = BigInt.fromI32(1)
